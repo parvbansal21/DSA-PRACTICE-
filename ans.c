f57079ff39fd49efbb35b3c1af78410c1d2a4c11
parvbansal21 <parv.bansal2024@vitstudent.ac.in>
2025-11-25 01:57:26 +0530

    Add ans.c C++ solution

---
+++ ans.c
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Printing array
+    cout << "Array elements: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    // Sum, max, min
+    int sum = 0;
+    int maxi = arr[0];
+    int mini = arr[0];
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+        maxi = max(maxi, arr[i]);
+        mini = min(mini, arr[i]);
+    }
+
+    cout << "\nSum = " << sum;
+    cout << "\nMax = " << maxi;
+    cout << "\nMin = " << mini;
+
+    return 0;
+}
