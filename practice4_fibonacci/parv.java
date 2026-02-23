5af76bbf5fedcb4f9f4f7149e474819163ba4ff8
Parv Bansal <parv.bansal2024@vitstudent.ac.in>
2026-02-24 00:44:23 +0530

    fibonacci

---
+++ practice4_fibonacci/parv.java
@@ -0,0 +1,16 @@
+public class parv {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        
+        // Fibonacci series
+        int n1 = 0, n2 = 1;
+        System.out.print("Fibonacci Series: " + n1 + " " + n2);
+        
+        for (int i = 2; i < 10; i++) {
+            int n3 = n1 + n2;
+            System.out.print(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
